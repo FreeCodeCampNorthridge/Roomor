83375767e2a0e6885c86c4f93eb731816b6a4185
T-Wesst <code.daddy17@gmail.com>
2019-03-04 10:53:44 -0800

    setup webpack, babel, react environment

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Roomor App</h1>
+        <p>hello there this is the roomor app</p>
+      </div>
+    );
+  }
+}
+export default App;
