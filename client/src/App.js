de45d918b7bc46d8fbb24f8b50966dd9c0701027
T-Wesst <code.daddy17@gmail.com>
2019-03-13 11:03:38 -0700

    boilerplate app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Landing from './pages/Index';
-import Home from './pages/Home';
-import About from './pages/About';
-import Footer from './components/Footer';
-import '@material-ui/core';
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path="/" Component={Landing} />
-            <Route path="/home" Component={Home} />
-            <Route path="/about" Component={About} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
+      <div className="App">
+        <h1>Roomor React App</h1>
+      </div>
     );
   }
 }
--- client/src/pages/Index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Index() {
-  return <div>landing page</div>;
-}
