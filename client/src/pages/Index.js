1c63293446ada714f3936e9a890b8a1dcc4f6344
T-Wesst <code.daddy17@gmail.com>
2019-03-12 22:13:19 -0700

    landing page

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,46 +1,5 @@
-import Login from '../components/Login';
-import Register from '../components/Register';
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary
-  }
-});
-
-function Landing(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={24}>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>
-            xs=12 sm=6
-            {/* app logo component here */}
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>
-            {/* render conditional component */}
-            {true ? <Register /> : <Login />}
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+export default function Index() {
+  return <div>landing page</div>;
 }
-
-Landing.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Landing);
