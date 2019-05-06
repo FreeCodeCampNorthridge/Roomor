c8ee0b2095e9ed1c93c42dd2a7b2ca93ab8167c8
T-Wesst <code.daddy17@gmail.com>
2019-05-06 16:23:24 -0700

    add CRUD routes

--- api/server.js
+++ api/server.js
@@ -1,35 +1,71 @@
 const express = require('express'),
+  mongojs = require('mongojs'),
   app = express(),
   PORT = process.env.PORT || 3000;
-// DATA ==============
-const users = [
-  { id: 1, name: 'wesley', age: 45, occupation: 'senior' },
-  { id: 2, name: 'mike', age: 22, occupation: 'student' },
-  { id: 3, name: 'kyle', age: 12, occupation: 'teenager' },
-  { id: 4, name: 'manny', age: 27, occupation: 'military' }
-];
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// database config
+// Save the URL of our database as well as the name of our collection
+var databaseUrl = 'roomor';
+var collections = ['users'];
+// hook mongojs to db variable
+const db = mongojs(databaseUrl, collections);
+// log mongoDB errors
+db.on('error', err => {
+  console.log('MongoDB Error', err);
+});
 // ROUTES ==================
-// root
-app.get('/', (req, res) => {
-  res.send('root route');
+// get users
+app.get('/api/users', (req, res) => {
+  db.users.find({}, (err, users) => {
+    if (err) throw err;
+    res.json(users);
+  });
 });
 // get user
 app.get('/api/users/:id', (req, res) => {
-  res.send('get single user');
+  const { id } = req.params;
+  const ObjectId = mongojs.ObjectId;
+  db.users.findOne({ _id: ObjectId(id) }, (err, user) => {
+    if (err) throw err;
+    res.json(user);
+  });
 });
-// get users
-app.get('/api/users', (req, res) => res.json(users));
 // create new user
 app.post('/api/users/new', (req, res) => {
-  res.send('add new user');
+  const { firstName, lastName, email, password } = req.body;
+  db.users.insert({ firstName, lastName, email, password }, (err, user) => {
+    if (err) throw err;
+    res.redirect('/api/users');
+  });
 });
 // update user
-app.put('/api/users/:id', (req, res) => {
-  res.send('update user');
+app.put('/api/users/:id/edit', (req, res) => {
+  const ObjectId = mongojs.ObjectId;
+  const { id } = req.params;
+  const { email, password, firstName, lastName } = req.body;
+  console.log(id, email, password, firstName, lastName);
+  db.users.findAndModify(
+    {
+      query: { _id: ObjectId(id) },
+      update: { $set: { email, password, firstName, lastName } },
+      new: true
+    },
+    (err, user) => {
+      if (err) throw err;
+      res.redirect('/api/users');
+    }
+  );
 });
 // delete user
 app.delete('/api/users/:id', (req, res) => {
-  res.send('delete user');
+  let ObjectId = mongojs.ObjectId;
+  const { id } = req.params;
+  db.users.remove({ _id: ObjectId(id) }, err => {
+    if (err) throw err;
+    res.redirect('/api/users');
+  });
 });
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
