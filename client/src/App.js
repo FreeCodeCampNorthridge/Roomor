4432d7179636c7cb5660e33eab47ed4b9df5d4e0
T-Wesst <code.daddy17@gmail.com>
2019-05-07 14:06:08 -0700

    add unauthenticated login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  // empty state
+  state = {};
+  // on submit
+  handleSubmit = () => {
+    // post state to login route
+    axios
+      .post('/api/users/login', this.state)
+      // set state with response from server
+      .then(response => {
+        this.setState({ user: response.data.user }, () =>
+          // log current state
+          console.log(this.state)
+        );
+      })
+      // throw error if something went wrong
+      .catch(err => {
+        if (err) throw err;
+      });
+  };
+  // on event
+  handleChange = (e, key) => {
+    // grab event value
+    let value = e.target.value;
+    // set state of key equal to vale and log
+    this.setState({ [key]: value }, () => console.log(this.state));
+  };
+
   render() {
-    return <div className="App">Roomor App</div>;
+    return (
+      <div>
+        <h1>Roomor App</h1>
+        <input
+          type="text"
+          onChange={e => this.handleChange(e, 'username')}
+          placeholder="username"
+        />
+        <input
+          type="text"
+          onChange={e => this.handleChange(e, 'email')}
+          placeholder="email"
+        />
+        <input
+          type="text"
+          onChange={e => this.handleChange(e, 'password')}
+          placeholder="password"
+        />
+        <button onClick={this.handleSubmit} value="Submit" />
+      </div>
+    );
   }
 }
-
 export default App;
--- server/controllers/userController/login.js
+++ server/controllers/userController/login.js
@@ -2,7 +2,14 @@ const { User } = require('../../models');
 
 module.exports = function(req, res) {
   console.log('hit login');
-  User.findById(req.params.id)
-    .then(user => res.json(user))
-    .catch(err => res.json(err));
+  res.send({
+    user: {
+      username: 'fook ya',
+      email: 'food@food.com',
+      password: 'password'
+    }
+  });
+  // User.findById(req.params.id)
+  //   .then(user => res.json(user))
+  //   .catch(err => res.json(err));
 };
