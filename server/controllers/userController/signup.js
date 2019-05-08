250586a7530594488e8bcb9039e83b719a4b4707
T-Wesst <code.daddy17@gmail.com>
2019-05-08 12:49:35 -0700

    add signup route

--- server/controllers/userController/signup.js
+++ server/controllers/userController/signup.js
@@ -1,9 +1,20 @@
 const { User } = require('../../models');
+const { createToken } = require('./tokenService');
 
 module.exports = function(req, res) {
   User.create(req.body)
-    .then(({ firstName, lastName }) =>
-      res.send({ message: `${firstName} ${lastName} was created ` })
-    )
-    .catch(err => res.send({ message: err }));
+    .then(user => {
+      let token = createToken(user);
+      res.cookie('token', token, {
+        // secure: true, on deployment uncomment
+        signed: true,
+        maxAge: 1000 * 60 * 60,
+        httpOnly: true // on dev
+      });
+      res.send({ message: `${user} was created` });
+      // res.redirect('/users/someAuthorizedRoute')
+    })
+    .catch(err => {
+      if (err) throw err;
+    });
 };
