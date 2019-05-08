03cdcd440e71d05644df9d4be2ea5045005ba22e
T-Wesst <code.daddy17@gmail.com>
2019-05-08 12:43:31 -0700

    add dependencies

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const express = require('express'),
-  mongoose = require('mongoose'),
   app = express(),
-  PORT = process.env.PORT || 3001;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  cookieParser = require('cookie-parser'),
+  { config } = require('dotenv'),
+  logger = require('morgan'),
+  { urlencoded, json } = express;
+// parses incoming requests with JSON payloads
+app.use(urlencoded({ extended: true }));
+app.use(json());
+// load env variable
+config();
+// parse cookies and puts information onto req object
+// encodes and decrypts with provided SECRET
+app.use(cookieParser(process.env.SECRET));
 // ============ MongoDB config ==============
-const url = process.env.DATABASEURL || 'mongodb://localhost/roomor';
-mongoose.connect(url, { useNewUrlParser: true });
+require('./config/connection');
+// log response status
+app.use(logger('dev'));
 // ============= ROUTES ==========================
 app.use('/api/users', require('./routes/userRoutes.js'));
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(process.env.PORT || 3001);
