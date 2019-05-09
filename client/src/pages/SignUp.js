579c66806bc13e045a8909ad91dd83484bfc1f67
T-Wesst <code.daddy17@gmail.com>
2019-05-09 11:33:11 -0700

    add material components

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
 import axios from 'axios';
 
 export default class SignUp extends Component {
   state = {};
-  handleChange = (event, nameAttr) => {
-    this.setState({ [nameAttr]: event.target.value });
+  handleChange = ({ target: { value } }, name) => {
+    this.setState({ [name]: value });
   };
-
   handleSubmit = () => {
     axios
       .post('/api/users/signup', this.state)
       .then(user => {
         this.setState({ user });
-        console.log(this.state);
       })
       .catch(err => {
         if (err) throw err;
@@ -21,40 +23,66 @@ export default class SignUp extends Component {
   render() {
     return (
       <div>
-        <h1>Roomor App: Register</h1>
-        <input
-          type="text"
-          placeholder="enter a username"
-          name="username"
-          onChange={event => this.handleChange(event, event.target.name)}
-        />
-        <input
-          type="text"
-          onChange={event => this.handleChange(event, event.target.name)}
-          placeholder="email"
-          name="email"
-        />
-        <input
-          type="text"
-          onChange={event => this.handleChange(event, event.target.name)}
-          placeholder="lastName"
-          name="lastName"
-        />
-        <input
-          type="text"
-          onChange={event => this.handleChange(event, event.target.name)}
-          placeholder="firstName"
-          name="firstName"
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="enter a password"
-          onChange={event => this.handleChange(event, event.target.name)}
-        />
-        <button onClick={this.handleSubmit} value="submit">
-          Submit
-        </button>
+        <FormControl>
+          <InputLabel htmlFor="username">username</InputLabel>
+          <Input
+            type="text"
+            id="username"
+            name="username"
+            autoFocus
+            onChange={event => this.handleChange(event, 'username')}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="email">email</InputLabel>
+          <Input
+            id="email"
+            type="text"
+            onChange={event => this.handleChange(event, 'email')}
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="lastName">lastName</InputLabel>
+          <Input
+            id="lastName"
+            type="text"
+            onChange={event => this.handleChange(event, 'lastName')}
+            name="lastName"
+            autoFocus
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="firstName">firstName</InputLabel>
+          <Input
+            id="firstName"
+            type="text"
+            onChange={event => this.handleChange(event, 'firstName')}
+            name="firstName"
+            autoFocus
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="password">password</InputLabel>
+          <Input
+            id="password"
+            type="text"
+            name="password"
+            autoFocus
+            autoComplete="current-password"
+            onChange={event => this.handleChange(event, 'password')}
+          />
+        </FormControl>
+        <Button
+          onClick={this.handleSubmit}
+          color="primary"
+          variant="contained"
+          type="submit"
+        >
+          Register
+        </Button>
       </div>
     );
   }
