d309a5b705128b130a793d7ce00eab4478c5cbe1
T-Wesst <code.daddy17@gmail.com>
2019-05-09 14:24:13 -0700

    add routes and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './pages/NotFound';
 import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 class App extends Component {
   render() {
     return (
-      <div>
-        <SignUp />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
