d8fdd5c745a4c8a2b567448886234e9f3f3e7b6e
T-Wesst <code.daddy17@gmail.com>
2019-05-14 11:44:43 -0700

    add login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Person from '@material-ui/icons/Person';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
+import axios from 'axios';
 
 const styles = theme => ({
   main: {
@@ -45,24 +44,43 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3
   }
 });
+class Login extends Component {
+  state = {};
+  handleChange = ({ target: { value } }, name) => {
+    this.setState({ [name]: value });
+  };
+  handleSubmit = () => {
+    axios
+      .post('/api/users/login', this.state)
+      .then(user => {
+        this.setState({ user });
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <main className={classes.main}>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <Person />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log In
+          </Typography>
 
-function SignIn(props) {
-  const { classes } = props;
-
-  return (
-    <main className={classes.main}>
-      <CssBaseline />
-      <Paper className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <Person />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Log in
-        </Typography>
-        <form className={classes.form}>
           <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus />
+            <InputLabel htmlFor="username">Username </InputLabel>
+            <Input
+              id="username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={event => this.handleChange(event, 'username')}
+            />
           </FormControl>
           <FormControl margin="normal" required fullWidth>
             <InputLabel htmlFor="password">Password</InputLabel>
@@ -70,30 +88,28 @@ function SignIn(props) {
               name="password"
               type="password"
               id="password"
+              onChange={event => this.handleChange(event, 'password')}
               autoComplete="current-password"
             />
           </FormControl>
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={this.handleSubmit}
           >
-            Sign in
+            Log In
           </Button>
-        </form>
-      </Paper>
-    </main>
-  );
+        </Paper>
+      </main>
+    );
+  }
 }
 
-SignIn.propTypes = {
+Login.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(SignIn);
+export default withStyles(styles)(Login);
