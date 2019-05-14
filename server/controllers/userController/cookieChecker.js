43b3ed6c969fdb2d2df99d11e6a30c6b312515c0
T-Wesst <code.daddy17@gmail.com>
2019-05-13 22:55:27 -0700

    add authorization

--- server/controllers/userController/cookieChecker.js
+++ server/controllers/userController/cookieChecker.js
@@ -2,29 +2,21 @@ const { User } = require('../../models'),
   { validToken } = require('../../utilities/tokenService');
 
 const cookieChecker = async function(req, res) {
-  console.log(req.signedCookies);
   try {
-    // /authorized route takes signedCookies from request object and token from signedCookies
-    // console.log(req.cookies);
-    // returns empty object
-    // if (token) {
-    //   try {
-    //     // tokenService.validToken(token).then(user)
-    //     // check that user in db with both values exists (hardcoded hashed password) (would never get to this point anyways but serves as extra security)
-    //     let {
-    //       data: { _id, username, password, email }
-    //     } = await validToken(token);
-    //     // grab username and hashed password from user from token
-    //     let user = await User.findOne({ username, password, email, _id });
-    //     // get user back from db
-    //     // send user.username and user.email back to front end
-    //     res.send({ email: user.email, username: user.username });
-    //   } catch (err) {
-    //     if (err) throw err;
-    //   }
-    // } else {
-    //   res.send('Your cookie expired, please log back in');
-    // }
+    // get token from signedCookies from request object
+    let { token } = req.signedCookies;
+    if (token) {
+      // validate token and grab username and hashed password from user from token
+      let {
+        user: { username, password }
+      } = await validToken(token);
+      // get user back from db
+      let user = await User.findOne({ username, password });
+      // send user.username and user.email back to front end
+      res.send({ email: user.email, username: user.username });
+    } else {
+      res.send('Your cookie expired, please log back in');
+    }
   } catch (err) {
     if (err) throw err;
   }
